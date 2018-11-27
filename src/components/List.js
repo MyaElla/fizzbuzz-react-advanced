62b198d3d81634379c66400089594bb6448e6c8f
Myaella <mya17plus@yahoo.ca>
2018-11-27 17:39:44 +0000

    create files for rendering fizz buzz list of numbers

---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Fizz from './Fizz'
+
+export default class List extends Component {
+  render() {
+    return (
+      <ul>
+        <Fizz/>
+      </ul>
+    )
+  }
+}
