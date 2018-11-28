3a4fda5dd617c8817d868c7e2138a5449ebe03ef
Myaella <mya17plus@yahoo.ca>
2018-11-28 17:15:34 +0000

    replace list with counter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import List from "./components/List";
+import Counter from "./components/Counter";
 import "./App.css";
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         
-          <List />
+          <Counter />
         
       </div>
     );
