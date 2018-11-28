1668f3b208ead2c4147cfa928c01baad75d64a9e
Myaella <mya17plus@yahoo.ca>
2018-11-28 17:39:00 +0000

    updating the test for user story

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -6,9 +6,13 @@ describe("Counter component", () => {
 
   it("starts with a count of 0", () => {
     const wrapper = shallow(<Counter />);
-    const countState = wrapper.state().count
-    expect(countState).toEqual(0)
+    // one function from enzyme is 'state()' - gets the state of a component
+    // const countState = wrapper.state().count
+    // expect(countState).toEqual(0)
 
+    // from a user's perspective
+    const text = wrapper.find("p").text();
+    expect(text).toEqual("Current count: 0");
   });
 
 });
